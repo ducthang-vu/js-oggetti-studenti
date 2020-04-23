6b9131ec456b37356e6cd205ee2b39e7be73406d
Duc Thang Vu <vuducthang91@gmail.com>
2020-04-23 14:19:23 +0200

    main task completed

---
+++ script/main.js
@@ -0,0 +1,15 @@
+console.log('main.js is working');
+
+
+var student = {
+    nome: 'Tizio',
+    cognome: 'Caio',
+    età: '25'
+}
+
+
+console.log(student)
+
+for (var key in student) {
+    console.log(key + ': ' + student[key])
+}
